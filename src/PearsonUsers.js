2912180c078ed8287089b1599c6a413445e17da4
Mohit Gupta <mohit.gupta3@pearson.com>
2018-08-28 14:54:41 +0530

    Added Output screen and removed unused package.

--- src/PearsonUsers.js
+++ src/PearsonUsers.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import classNames from 'classnames';
 import userService from './services/users';
 import UserCard from './components/UserCard';
 import Loader from './components/Loader';
